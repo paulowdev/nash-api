f4e9936abb370f20eba9aa49d8f1175c946c41c0
Gean Júnior <gean.adm@gmail.com>
2014-03-24 15:32:35 -0300

    Escondendo senha do arquivo de configuração dos testes.

--- config-template.php
+++ config-template.php
@@ -4,17 +4,17 @@ return array (
     "running" => require("env.php"),
     "development" => array(
         "servicePath" => "http://localhost:2140/nash",
-        "username" => "site",
-        "password" => "4321"
+        "username" => "username",
+        "password" => "******"
     ),
     "statement" => array(
         "servicePath" => "https://srvaramis/nash",
-        "username" => "site",
-        "password" => "4321"
+        "username" => "username",
+        "password" => "******"
     ),
     "production" => array(
-        "servicePath" => "http://nash.fortesinformatica.com.br",
-        "username" => "site",
-        "password" => "4321"
+        "servicePath" => "https://nash.fortesinformatica.com.br",
+        "username" => "username",
+        "password" => "******"
     )
 );
