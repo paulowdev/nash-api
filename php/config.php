3f9f239e0fe4cd57903dfb1975f5f93e8237cbac
Gean Júnior <gean.adm@gmail.com>
2014-12-16 18:32:11 -0300

    Ajustes na configuração para os testes.

--- php/config.php
+++ php/config.php
@@ -3,12 +3,14 @@
 return array (
     "running" => require("env.php"),
     "development" => array(
+        "authenticationPath" => "http://localhost:30627",
         "servicePath" => "http://localhost:31877",
         "username" => "site",
-        "password" => "4321"
+        "password" => "site"
     ),
     "statement" => array(
-        "servicePath" => "https://srvaramis/nash",
+        "authenticationPath" => "http://nash-comercial.elasticbeanstalk.com:8081",
+        "servicePath" => "http://nash-comercial.elasticbeanstalk.com",
         "username" => "site",
         "password" => "4321"
     )
