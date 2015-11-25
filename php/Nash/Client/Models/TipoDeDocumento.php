9700043bf4642e7ad6fa907df04bbec6dcb6968f
Rubens Gadelha <rubensgadelha@gmail.com>
2015-11-25 12:37:06 -0300

    Adição de services e models para criação de conta a receber

---
+++ php/Nash/Client/Models/TipoDeDocumento.php
@@ -0,0 +1,19 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+require_once dirname(realpath(__FILE__)) . '/Entidade.php';
+
+/**
+ * Description of Conta
+ *
+ * @author rubensgadelha
+ */
+class TipoDeDocumento extends Entidade {
+
+    public $Nome;
+}
--- php/Nash/Client/Services/ContaAReceberService.php
+++ php/Nash/Client/Services/ContaAReceberService.php
@@ -11,33 +11,33 @@
  *
  * @author elvislima
  */
-//class ContaAReceberService extends AbstractCrudService {
-//    public function entityName() {
-//        return "contaareceber";
-//    }
-//
-//    public function entityClassName() {
-//        return "ContaAReceber";
-//    }
-//    
-//    public function create(Entidade $entity) {
-//        $this->session->contentType = "application/json";  
-//        
-//        try {
-//           $json = $entity->toJson();    
-//        } catch (Exception $ex) {
-//            var_dump($ex);
-//        }
-//        
-//        $result = $this->session->post("/{$this->entityName()}/inclui", $json);        
-//        return $result;
-//    }
-//    
-//    public function update(Entidade $entity) {
-//        $this->session->contentType = "application/json";  
-//        $json = $entity->toJson();
-//        
-//        $result = $this->session->put("/{$this->entityName()}/altera/{$entity->getId()}", $json);        
-//        return $result;
-//    }
-//}
+class ContaAReceberService extends AbstractCrudService {
+    public function entityName() {
+        return "contaareceber";
+    }
+
+    public function entityClassName() {
+        return "ContaAReceber";
+    }
+
+    public function create(Entidade $entity) {
+        $this->session->contentType = "application/json";
+
+        try {
+           $json = $entity->toJson();
+        } catch (Exception $ex) {
+            var_dump($ex);
+        }
+
+        $result = $this->session->post("/{$this->entityName()}/inclui", $json);
+        return $result;
+    }
+
+    public function update(Entidade $entity) {
+        $this->session->contentType = "application/json";
+        $json = $entity->toJson();
+
+        $result = $this->session->put("/{$this->entityName()}/altera/{$entity->getId()}", $json);
+        return $result;
+    }
+}
